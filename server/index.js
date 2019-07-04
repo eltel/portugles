34c86bc5c92c902721714f45774f32009a17e97e
Terry Mitchell <djterrymitchell@gmail.com>
2019-07-04 17:55:47 -0300

    server.js tweak for service worker v2

--- server/index.js
+++ server/index.js
@@ -61,20 +61,20 @@ app
     //   const parsedUrl = parse(req.url, true);
     //   const { pathname } = parsedUrl;
 
-      // handle GET request to ../service-worker.js
-      server.get("/service-worker.js", function(request, response) {
-        response.sendFile(
-          path.resolve(__dirname, "../.next", "service-worker.js")
-        );
-      });
-      // if (pathname === "../service-worker.js") {
-      //   const filePath = join(__dirname, ".next", pathname);
-      //
-      //   app.serveStatic(req, res, filePath);
-      // } else {
-      //   handle(req, res, parsedUrl);
-      // }
+    // handle GET request to ../service-worker.js
+    server.get("/service-worker.js", function(request, response) {
+      response.sendFile(
+        path.resolve(__dirname, "../.next", "service-worker.js")
+      );
     });
+    // if (pathname === "../service-worker.js") {
+    //   const filePath = join(__dirname, ".next", pathname);
+    //
+    //   app.serveStatic(req, res, filePath);
+    // } else {
+    //   handle(req, res, parsedUrl);
+    // }
+    //  });
 
     server.use("/api/v1/books", bookRoutes);
     server.use("/api/v1/portfolios", portfolioRoutes);
